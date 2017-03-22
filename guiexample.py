1dd84061c7ef70ee0013a9091d962e49259929ed
Ryuji Tanaka <Ryuji.Tanaka@arm.com>
2017-03-22 17:06:21 +0900

    simplify

--- guiexample.py
+++ guiexample.py
@@ -1,25 +1,21 @@
 from arm_ds.debugger_v1 import Debugger
 from arm_ds.debugger_v1 import DebugException
-from javax.swing import (BoxLayout, ImageIcon, JButton, JFrame, JPanel,
-        JPasswordField, JLabel, JTextArea, JTextField, JScrollPane,
-        SwingConstants, WindowConstants)
-from java.awt import Component, GridLayout
+from javax.swing import JButton, JFrame, JPanel, JLabel, JTextField
 
-class guitest(object):
+class gui_example(object):
 
     def __init__(self):
-        self.frame = JFrame("GUI Test")
-        self.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE) # EXIT_ON_CLOSE will terminate Eclipse
+        self.frame = JFrame("GUI example")
 
-        self.loginPanel = JPanel(GridLayout(0,2))
-        self.frame.add(self.loginPanel)
+        self.dscmdPanel = JPanel()
+        self.frame.add(self.dscmdPanel)
 
-        self.dscmdField = JTextField('',15)
-        self.loginPanel.add(JLabel("DS Command:", SwingConstants.RIGHT))
-        self.loginPanel.add(self.dscmdField)
+        self.dscmdField = JTextField('',30)
+        self.dscmdPanel.add(JLabel("DS Command:"))
+        self.dscmdPanel.add(self.dscmdField)
 
-        self.loginButton = JButton('Run',actionPerformed=self.rundscmd)
-        self.loginPanel.add(self.loginButton)
+        self.dscmdButton = JButton('Run',actionPerformed=self.rundscmd)
+        self.dscmdPanel.add(self.dscmdButton)
 
         self.frame.pack()
         self.frame.setVisible(True)
@@ -27,12 +23,12 @@ class guitest(object):
     def rundscmd(self,event):
         dscmd = self.dscmdField.text
         print dscmd
-        
+
         # Obtain the first execution context
         debugger = Debugger()
         ec = debugger.getCurrentExecutionContext()
         ec.executeDSCommand(dscmd)
 
 if __name__ == '__main__':
-    print "GUI test"
-    guitest() 
+    print "GUI example"
+    gui_example() 
